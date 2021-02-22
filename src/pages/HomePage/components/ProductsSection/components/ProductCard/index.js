8c44e0835ea036c0d2a2c03e90a1d9ed21f2e95c
herickmotta <herickrossato@gmail.com>
2021-02-22 02:02:00 -0300

    feat: change buy to nostock when avaiable is 0

--- src/pages/HomePage/components/ProductsSection/components/ProductCard/index.js
+++ src/pages/HomePage/components/ProductsSection/components/ProductCard/index.js
@@ -37,7 +37,7 @@ export default function ProductCard({ product }) {
       <Button
         width="100%"
         height="15%"
-        text="BUY"
+        text={available > 0 ? "BUY" : "NO STOCK"}
         onClick={addProductToCart}
         loading={loading}
         disabled={disabled}
